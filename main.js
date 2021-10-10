7823d295b713af7156b645c9873aa4ab166c7663
James Moore <james816am@gmail.com>
2021-10-10 15:23:31 -0400

    Basic functionality in place

---
+++ main.js
@@ -0,0 +1,135 @@
+// let alphabet=['Q','W','E','R','T','Y','U','I','O','P','A','S','D','F','G','H','J','K','L','Z','X','C','V','B','N','M'];
+let A = document.getElementById('A');
+let B = document.getElementById('B');
+let C = document.getElementById('C');
+let D = document.getElementById('D');
+let E = document.getElementById('E');
+let F = document.getElementById('F');
+let G = document.getElementById('G');
+let H = document.getElementById('H');
+let I = document.getElementById('I');
+let J = document.getElementById('J');
+let K = document.getElementById('K');
+let L = document.getElementById('L');
+let M = document.getElementById('M');
+let N = document.getElementById('N');
+let O = document.getElementById('O');
+let P = document.getElementById('P');
+let Q = document.getElementById('Q');
+let R = document.getElementById('R');
+let S = document.getElementById('S');
+let T = document.getElementById('T');
+let U = document.getElementById('U');
+let W = document.getElementById('V');
+let V = document.getElementById('W');
+let X = document.getElementById('X');
+let Y = document.getElementById('Y');
+let Z = document.getElementById('Z');
+let mysteryWord = document.getElementById('mysteryWord');
+let guessDisplay = document.getElementById('guesses')
+guessesRemaining = 5;
+guessDisplay.innerText = guessesRemaining;
+
+let deadLetters = document.getElementById('deadLetters');
+let chosenWord = words[Math.floor(Math.random() * words.length)]
+chosenWord = chosenWord.toUpperCase();
+let wordDisplay = Array.from(chosenWord);
+wordDisplay = wordDisplay.fill('_');
+
+mysteryWord.innerText = wordDisplay.join(" ");
+
+function checkLetter(letter) {
+    if (guessesRemaining > 0 && deadLetters.innerText.includes(letter) === false) {
+        for (let i = 0; i < chosenWord.length; i++) {
+            if (letter === chosenWord[i]) {
+                wordDisplay[i] = letter;
+            }
+        }
+        mysteryWord.innerText = wordDisplay.join(" ")
+        if (wordDisplay.includes(letter) === false) {
+            deadLetters.innerText += letter;
+            guessesRemaining--;
+            guessDisplay.innerText = guessesRemaining;
+        }
+    }
+}
+
+
+A.addEventListener('click', () => {
+    checkLetter('A');
+})
+B.addEventListener('click', () => {
+    checkLetter('B');
+})
+C.addEventListener('click', () => {
+    checkLetter('C');
+})
+D.addEventListener('click', () => {
+    checkLetter('D');
+})
+E.addEventListener('click', () => {
+    checkLetter('E');
+})
+F.addEventListener('click', () => {
+    checkLetter('F');
+})
+G.addEventListener('click', () => {
+    checkLetter('G');
+})
+H.addEventListener('click', () => {
+    checkLetter('H');
+})
+I.addEventListener('click', () => {
+    checkLetter('I');
+})
+J.addEventListener('click', () => {
+    checkLetter('J');
+})
+K.addEventListener('click', () => {
+    checkLetter('K');
+})
+L.addEventListener('click', () => {
+    checkLetter('L');
+})
+M.addEventListener('click', () => {
+    checkLetter('M');
+})
+N.addEventListener('click', () => {
+    checkLetter('N');
+})
+O.addEventListener('click', () => {
+    checkLetter('O');
+})
+P.addEventListener('click', () => {
+    checkLetter('P');
+})
+Q.addEventListener('click', () => {
+    checkLetter('Q');
+})
+R.addEventListener('click', () => {
+    checkLetter('R');
+})
+S.addEventListener('click', () => {
+    checkLetter('S');
+})
+T.addEventListener('click', () => {
+    checkLetter('T');
+})
+U.addEventListener('click', () => {
+    checkLetter('U');
+})
+V.addEventListener('click', () => {
+    checkLetter('V');
+})
+W.addEventListener('click', () => {
+    checkLetter('W');
+})
+X.addEventListener('click', () => {
+    checkLetter('X');
+})
+Y.addEventListener('click', () => {
+    checkLetter('Y');
+})
+Z.addEventListener('click', () => {
+    checkLetter('Z');
+})
